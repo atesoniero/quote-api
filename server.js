4ecc709325d5ea4471bfb2f324803cd9532b64f1
Andrea Tesoniero <andrea.tesoniero@gmail.com>
2021-03-02 10:36:48 +0100

    Beautify and Add comments

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
-const { quotes } = require('./data');
-const { getRandomElement } = require('./utils');
+const {
+    quotes
+} = require('./data');
+const {
+    getRandomElement
+} = require('./utils');
 
 const PORT = process.env.PORT || 4001;
 
@@ -10,24 +14,32 @@ app.use(express.static('public'));
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`)
-  });
-  
+});
+
 // Get a random quote
 app.get('/api/quotes/random', (req, res, next) => {
     const randomQuote = getRandomElement(quotes);
-        res.send(randomQuote.quote);
+    res.send({
+        quote: randomQuote.quote
+    });
 });
 
 app.get('/api/quotes', (req, res, next) => {
     const arrayQuotes = []
     // if no ?person= query
     if (!req.query.person) {
-       quotes.forEach(element => {arrayQuotes.push(element.quote)}) 
-       res.send(arrayQuotes)
-    } else { // if ?person= query is given: it will send the quotes if the person exists or send an empy array of it does not
-        quotes.filter(element => {  return (element.person === req.query.person) })
-        .forEach(element => {arrayQuotes.push(element.quote)});
-        res.send(arrayQuotes)    
+        quotes.forEach(element => {
+            arrayQuotes.push(element.quote)
+        })
+        res.send(arrayQuotes)
+    } else { // if ?person= query is given: it will send the quotes or send back an empy array of it does not
+        quotes.filter(element => {
+                return (element.person === req.query.person)
+            })
+            .forEach(element => {
+                arrayQuotes.push(element.quote)
+            });
+        res.send(arrayQuotes)
     }
 })
 
@@ -36,7 +48,10 @@ app.post('/api/quotes', (req, res, next) => {
     const person = req.query.person;
     const quote = req.query.quote;
     if (person && quote) {
-        const newQuote = { quote: quote, person: person};
+        const newQuote = {
+            quote: quote,
+            person: person
+        };
         quotes.push(newQuote)
         res.send(newQuote.quote)
     } else {
